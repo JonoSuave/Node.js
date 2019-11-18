5512365ca8cc227646a44de5f7775d8a3a12c69a
JonoSuave <JonoSuave@gmail.com>
2019-11-18 16:39:15 -0700

    Fetching forecast
    Noticed that app crashes when I try an invalid search of '123kh'

--- node-course/web-server/public/js/app.js
+++ node-course/web-server/public/js/app.js
@@ -1,1 +1,28 @@
 console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const weatherSearch = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    // prevent default form behavior, which is to refresh page
+    e.preventDefault()
+    const inputValue = weatherSearch.value
+    messageOne.textContent = `Just a sec, Boss`
+    messageTwo.textContent = ''
+    // console.log(weatherSearch)
+    fetch(`http://localhost:3000/weather?address=${inputValue}`)
+    .then((response) => {
+        response.json()
+        .then((data) => {
+            if(data.error) {
+                messageOne.textContent(data.error)
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = `${data.forecastData.currTemp} degrees, currently`
+            }
+  
+        })
+    })
+})
--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -21,7 +21,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        headline: `Welcome to the home of Weather, homey`,
+        headline: `Yo, Weather Homey`,
         name: 'Homey'
     })
 })
